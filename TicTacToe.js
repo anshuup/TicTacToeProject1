eca2b9604852ccd606a8f9d8ecc7dc7edabbff99
anshuup <anshupatel@cmail.carleton.ca>
2021-10-23 15:31:13 -0400

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,7 +2,6 @@ function alertWindow(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-		 document.getElementById("header").innerHTML = "hello, there"
 		}
 	};
 	xhttp.open("GET", "/", true);
