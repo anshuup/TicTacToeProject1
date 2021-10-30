56fd3f07ef2bd18a51de85e3c0065f8e053f7303
anshuup <anshupatel@cmail.carleton.ca>
2021-10-30 00:38:14 -0400

    Update TicTacToe.js

--- TicTacToe.js
+++ TicTacToe.js
@@ -49,12 +49,15 @@ function drawShape(boxClicked,boxId){
 	  setTimeout(function(){circleElem.classList.remove("elementToFadeInAndOut");}, 500);
 	}
 
+
 	if(document.getElementById("box"+boxId).innerText.length != 0){
 		setTimeout(function(){
-			console.log("winner "+winner)
-			console.log("circle score "+circleWinner)
-			console.log("cross score "+crossWinner)
 			if(winner == undefined){
+				let bestMoveArray = []
+
+				bestMoveArray = findBestPossibleMove()
+
+
 				let emptyValI = []
 				let emptyValJ = []
 
@@ -66,9 +69,19 @@ function drawShape(boxClicked,boxId){
 						}
 					}
 				}
-				let index = Math.floor(Math.random() * emptyValI.length);
-				let val = emptyValI[index]
-				let val2 = emptyValJ[index]
+				let val
+				let val2
+				//console.log(bestMoveArray)
+				if(bestMoveArray[0].length != 0){
+					val = bestMoveArray[0][0]
+					val2 = bestMoveArray[1][0]
+				}
+				else{
+					let index = Math.floor(Math.random() * emptyValI.length);
+					val = emptyValI[index]
+					val2 = emptyValJ[index]
+				}
+
 
 				document.getElementById("winnerVal").innerHTML = "Circle is making move..."
 				let checkElem = document.createElement("div")
@@ -88,7 +101,6 @@ function drawShape(boxClicked,boxId){
 					leftStyle = (parseInt(boxClicked)-6)*8 + 40
 					circleMove[val][val2] = -1
 				}
-				let topStyle = rowNum * 20 + 20
 				document.getElementById("box"+val+val2).appendChild(checkElem)
 				document.getElementById("box"+val+val2).style.color = "antiquewhite"
 				checkElem.style.display = "block"
@@ -96,7 +108,7 @@ function drawShape(boxClicked,boxId){
 				document.getElementById("box"+val+val2).onclick = false
 				findWinner("'"+val+val2+"'")
 				checkElem.classList.add("elementToFadeInAndOut");
-				setTimeout(function(){checkElem.classList.remove("elementToFadeInAndOut");}, 500);
+				setTimeout(function(){checkElem.classList.remove("elementToFadeInAndOut");}, 5000);
 			}
 			else{
 				/*document.body.addEventListener("click", function (evt) {
@@ -108,7 +120,7 @@ function drawShape(boxClicked,boxId){
 					}
 				});*/
 			}
-		}, 300);
+		}, 2000);
 	}
 }
 function twoPlay(boxClicked,boxId){
@@ -142,7 +154,8 @@ function twoPlay(boxClicked,boxId){
 		circleElem.style.display = "block"
 		circleElem.innerHTML ="O"
 		document.getElementById("box"+boxId).onclick = false
-
+		circleElem.classList.add("elementToFadeInAndOut");
+		setTimeout(function(){circleElem.classList.remove("elementToFadeInAndOut");}, 500);
 	}
 	else{
 		document.getElementById("winnerVal").innerHTML = "Circle is making move..."
@@ -164,10 +177,11 @@ function twoPlay(boxClicked,boxId){
 		checkElem.style.display = "block"
 		checkElem.innerHTML = "X"
 		document.getElementById("box"+boxId).onclick = false
+		checkElem.classList.add("elementToFadeInAndOut");
+		setTimeout(function(){checkElem.classList.remove("elementToFadeInAndOut");}, 500);
 	}
 }
 const arrayColumn = (arr, n) => arr.map(x => x[n]);
-
 
 function findWinner(boxId){
 	/* source https://stackoverflow.com/questions/16571035/javascript-tictactoe-if-winner-detection */
@@ -293,4 +307,84 @@ function findWinner(boxId){
 			}
 		}
 	}
+}
+
+
+function findBestPossibleMove(){
+	let bestMoveArrayRow = []
+	let bestMoveArrayColumn= []
+
+	console.log(circleMove)
+	a = circleMove.map((y) => y.reduce((a, b) => a + b));
+	b = circleMove.reduce((a, b) => a.map((v, i) => v + b[i]));
+
+
+	for(i=0;i<a.length;i++){
+		if(a[i] == 2){
+			bestMoveArrayRow.push(i)
+			for(j=0;j<circleMove[i].length;j++){
+				if(circleMove[i][j] == 0){
+					bestMoveArrayColumn.push(j)
+				}
+			}
+		}
+	}
+	for(i=0;i<b.length;i++){
+		if(b[i] == 2){
+			bestMoveArrayColumn.push(i)
+			for(j=0;j<circleMove[i].length;j++){
+				if(circleMove[j][i] == 0){
+					bestMoveArrayRow.push(j)
+				}
+			}
+		}
+	}
+
+	let diagonal1 = 0, diagonal2 = 0;
+	let diagnoalRow1
+	let diagnoalCol1
+
+	let diagnoalRow2
+	let diagnoalCol2
+
+	for (let row = 0; row < circleMove.length; row++) {
+		diagonal1 += circleMove[row][row];
+
+		if(diagonal1 == 2){
+			for(i=0;i<circleMove.length;i++){
+				if(circleMove[i][i] == 0){
+					diagnoalRow1 = i;
+					diagnoalCol1 = i;
+				}
+			}
+		}
+	}
+	for (let row = 0; row < circleMove.length; row++) {
+		diagonal2 += circleMove[row][circleMove.length - row - 1];
+
+		if(diagonal2 == 2 && circleMove[row][circleMove.length - row - 1] == 0){
+			for(i=0;i<circleMove.length;i++){
+				if(circleMove[i][circleMove.length - i - 1] == 0){
+					diagnoalRow2 = i;
+					diagnoalCol2 = circleMove.length - i - 1;
+				}
+			}
+		}
+	}
+	if(diagnoalRow1 != undefined){
+		bestMoveArrayRow.push(diagnoalRow1)
+		bestMoveArrayColumn.push(diagnoalRow1)
+	}
+	if(diagnoalRow2 != undefined){
+		bestMoveArrayRow.push(diagnoalRow2)
+		bestMoveArrayColumn.push(diagnoalCol2)
+	}
+	let array = []
+	array.push(bestMoveArrayRow)
+	array.push(bestMoveArrayColumn)
+	console.log(bestMoveArrayRow)
+	console.log(bestMoveArrayColumn)
+	return array
+
+	//find the one where computer wins i.e sum = -2
 }
